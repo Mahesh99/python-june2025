656c11d9d55bbc210f4a3c605cb511ce4b3d841e
Mahesh99 <maheshchary660@gmail.com>
2025-07-04 19:23:06 +0530

    new class

---
+++ hello2.py
@@ -0,0 +1,65 @@
+# print() is a built-in function
+print("Hello world")
+print("Welcome to python class")
+print(2+5)
+print(20+30)
+print(20-30)
+print(2*30)
+print(50/2)
+"""
+This is 
+a 
+multi-line 
+comment
+"""
+print(16%3)
+print(16//3)
+print(3**2)
+print(5**2)
+print(2**3)
+
+a=10
+print(a)
+b=20.3
+print(b)
+c="hi"
+print(c)
+
+name="sai"
+print(name)
+name2="kumar"
+print(name2)
+# is="harish"
+hero_name="Prabhas"
+print(hero_name)
+flower_color="red"
+print(flower_color)
+person_height=175
+print(person_height)
+cricket_score = 100
+print(cricket_score)
+print(person_height+cricket_score)
+
+cricket_score = cricket_score+4
+print(cricket_score)
+cricket_score = cricket_score+6
+print(cricket_score)
+cricket_score -= 5 
+print(cricket_score)
+x=10
+x*=2 #x=x*2
+print(x)#20
+x/=4 #x=x/4
+print(x)
+y=4
+y*=2 # y=4*2
+print(y)
+z=20
+z-=10
+print(z)
+m=45
+m*=2
+print(m)
+n=30
+n/=3
+print(n)
