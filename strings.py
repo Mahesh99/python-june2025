4a916da37299342666bc82f13d9a121f03ce2380
Mahesh99 <maheshchary660@gmail.com>
2025-07-08 14:55:47 +0530

    class

--- strings.py
+++ strings.py
@@ -83,3 +83,126 @@ print(g[0:8:3])
 print(g[::3])
 rev=g[::-1]
 print(rev)
+
+# int(), float()
+# str() is used to convert number types(int,float) to string
+print(int("12")+4)
+print(float("12.1")+4)
+price=2000
+fprice=price-(2000*10/100)
+print("Price after 10% discount is "+str(fprice))
+print("hi"+str(12))
+a=str(12) #"12"
+print(type(a))
+print(type(12))
+
+a=1
+b=1.1
+c="hi"
+e=str(a)
+f=str(b)
+print(type(a),a)
+print(type(b),b)
+print(type(c),c)
+print(type(e),e)
+print(type(f),f)
+
+# string methods
+# lower(), upper(), 
+text="PRAMANICUS ACADEMY"
+t2=text.lower() 
+print(text,t2)
+text2="hello world"
+t3=text2.upper()
+print(text2)
+print(t3)
+
+t4="hi".upper()
+print(t4)
+
+print("hi".upper())
+print(text2.title())
+print(text2.capitalize())
+print(text.capitalize())
+
+
+#count()
+
+print("Where there’s smoke, there’s fire".count(' ther'))
+print("Where there’s smoke, there’s fire".count('h'))
+print("Where there’s smoke, there’s fire".count('h',0,12))
+print("Where there’s smoke, there’s fire".find('h',4,12))
+print("Where there’s smoke, there’s fire".find('there'))
+print("Where there’s smoke, there’s fire".find('hi'))
+print()
+print("Where there’s smoke, there’s fire".index('h',4,12))
+print("Where there’s smoke, there’s fire".index('there'))
+# print("Where there’s smoke, there’s fire".index('hi'))
+
+# islower()
+text2="hello world"
+text="PRAMANICUS ACADEMY"
+print(text2.islower())
+print(text2.isupper())
+print(text.isupper())
+print("hi123".isalnum())
+print("hi123_".isalnum())
+print("hi123+".isalnum())
+print("hi123+".isdigit())
+print("123".isdigit())
+
+#format(),capitalize()
+
+name = "ARJUN"
+favourite_color = "Navy Blue"
+sentence = "{}'s favourite color is {}"
+# print(sentence)
+print(sentence.format(name,favourite_color))
+print(sentence.format("hi","hello"))
+
+
+# List
+l=[1,2,3,4]
+friends=["pranav","ashfaq","manuraj","likitha","sathvika"]
+print(type(l))
+print(l)
+n=len(l)
+print(n)
+print(len(friends))
+print(friends[2])
+print(friends[4])
+print(friends[-1])
+boys=friends[0:3]
+girls=friends[3:]
+print(boys)
+print(girls)
+print(friends[3])
+friends[3]="ruthika"
+print(friends)
+
+l=[1,2,3,4]
+l[0]=10
+print(l)
+l[2]=20
+print(l)
+
+# membership operators
+# in, not in
+print(1 in l)
+print(1 not in l)
+print("rishi" in boys)
+name="hari"
+print(name in boys)
+res="sai" not in boys
+print(res)
+
+# min(), max(), sum(), len(), join()
+l=[1,2,3,4]
+mn=min(l)
+mx=max(l)
+sm=sum(l)
+print(mn,mx,sm)
+
+l2=["seperate","all","list","strings","with","specified","string"]
+sent=" ".join(l2)
+print(sent)
