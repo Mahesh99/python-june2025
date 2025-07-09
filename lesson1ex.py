84f55636cd7fa0bfa1216ce81cea1c6cd0236349
MV2 <maheshchary660@gmail.com>
2025-07-09 19:48:32 +0530

    Create lesson1ex.py

---
+++ lesson1ex.py
@@ -0,0 +1,29 @@
+text = "HeLLo WoRLd"
+# Convert to lowercase
+
+text = "apple banana apple orange apple"
+# Count how many times 'apple' appears
+
+text = "python programming"
+# Capitalize only the first word
+
+text = "HeLLo WoRLd"
+# Convert to lowercase
+
+text = "HelloWorld"
+# slice 'Hello' in new variable s
+
+text = "Programming"
+# print last 3 characters
+
+text = "Python"
+# print 'nohtyP'
+
+items = [1, 2, 3, 4, 5]
+# reverse the list and print, refer string reverse notes.--> [5, 4, 3, 2, 1]
+
+text = "abcdefg"
+# Extract Every Second Character
+# print 'aceg'
+
+
