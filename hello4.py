c64e5d870af34cad5133f7c98d6289243ff41b73
MV2 <maheshchary660@gmail.com>
2025-07-15 19:02:14 +0530

    Add files via upload

---
+++ hello4.py
@@ -0,0 +1,22 @@
+# print() is a built-in function
+print("Hello student!") # i'm printing hello students
+print("Welcome to Pramanicus Academy")
+
+'''
+print(2+4)
+print(121+555)
+print(33-30)
+print(2*5)
+print(10/2)
+'''
+print(15/2)
+print(15//2)
+print(15%2)
+print(15%4)
+print(15%5)
+print(15%3)
+print(3**2)
+print(2**2)
+print(4**2)
+print(2**3)
+
