050c8e55c43ef5c5a580ffa506daf71da3f0443a
Mahesh99 <maheshchary660@gmail.com>
2025-07-25 11:51:17 +0530

    commit

---
+++ new batch/firstprogram.py
@@ -0,0 +1,52 @@
+#print("Hello world")
+print("Welcome to Pramanicus Academy")
+print("1+2")
+print(1+4)
+print(5-4)
+print(5*4)
+print(10/4)
+print(2314*34)
+print(12%3)
+print(12%5)
+print(5%15)
+print(10//5)
+print(10**2)
+print(5**2)
+print(2**3)
+
+a=1
+b=2
+print(a+b)
+c=a+b
+print(c)
+
+c=29
+f=1.8*c+32 # temperature conversion formulae
+print(f)
+
+"""
+cricket_score=141
+
+temp_in_c=29
+temp_in_f=84
+    
+print(temp_in_c)
+"""
+# with=20
+
+# Assignment operator
+# =, +=, -=, *=, /=, %=, //=, **=
+
+a=1
+# a=a+2
+a+=2 #3
+print(a)
+a-=1 #2
+print(a)
+a*=3 #6
+print(a)
+a/=6 #1
+print(a)
+a=3
+a**=3 
+print(a)
