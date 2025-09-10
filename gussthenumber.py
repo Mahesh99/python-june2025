88e397976129b6498bba3d07f5f03a2a9b242352
MV2 <maheshchary660@gmail.com>
2025-09-10 19:02:02 +0530

    Update gussthenumber.py

--- gussthenumber.py
+++ gussthenumber.py
@@ -1,6 +1,15 @@
-given_num=13
-n=int(input("guess the number:"))
-if given_num==n:
-    print("You guessed right")
-else:
-    print("You guessed wrong try again")
+r=random.randrange(1,101) #50
+name=input("Enter your name:")
+turns=0
+while True:
+  useri=int(input("Guess the number(1-100):"))
+  turns+=1
+  if useri==r:
+    print("You guessed right!!")
+    print(f"You took {turns} turns to guess right.")
+    break
+  elif r>useri:
+    print("Given number is larger. Try again")
+  elif r<useri:
+    print("Given number is smaller. Try again")
+
