6c679d48bc37f75ad20607971b1120a6027674b7
MV2 <maheshchary660@gmail.com>
2025-09-20 18:25:44 +0530

    Update oops.py

--- oops.py
+++ oops.py
@@ -142,3 +142,167 @@ class SuperHero(HumanBeing):
 sh1=SuperHero("Batman",36,"High Intelligence and advanced gadgets")
 sh1.introduce()
 sh1.show_power()
+
+
+
+
+# Magic methods or dunders(double underscore methods)
+class Employee:
+    # this method is called only once i.e when we create an object 
+    def __init__(self,name,age,salary,address):
+        # instance/object variables
+        self.name=name
+        self.age=age
+        self.salary=salary
+        self.address=address
+    
+    def update_salary(self,newsalary):
+        self.salary+=newsalary
+    def print_details(self):
+        print(f"Name:{self.name}\nAge:{self.age}\nSalary:{self.salary}\nAddress:{self.address}")
+    def __str__(self):
+        return f"{self.name}--{self.salary}"
+    def __add__(self,obj2):
+        # return f"{self.name}--{obj2.name}"
+        return self.salary+obj2.salary
+    def __len__(self):
+        return 1
+
+
+e=Employee("Hari",30,40000,"Uppal")
+e2=Employee("Ravi",25,25000,"Amberpet")
+print(e)
+print(e2)
+print(e+e2)
+print(len(e))
+
+
+
+print()
+print()
+
+l=[4,1,5,7,9]
+itr=iter(l)
+print(type(itr))
+
+# print(next(itr))
+# print(next(itr))
+# print(next(itr))
+# print(next(itr))
+# print(next(itr))
+# print(next(itr))
+
+# a=next(itr)
+# b=next(itr)
+# c=next(itr)
+# d=next(itr)
+# print(a,b,c,d)
+# e=next(itr)
+
+print()
+print()
+
+class MyIterator:
+    def __init__(self,n):
+        self.n = n
+
+    def __iter__(self):
+        return self
+        
+    def __next__(self):
+        if self.n == 0:
+            raise StopIteration()
+        temp = self.n
+        self.n -= 1
+        return temp
+    
+miter = MyIterator(6)
+i = iter(miter)
+print(next(miter))
+print(next(miter))
+print(next(miter))
+print(next(miter))
+print(next(miter))
+print(next(miter))
+# print(next(miter))
+
+miter = MyIterator(10)
+for i in miter:
+    print(i)
+
+print('hello')
+
+print()
+print()
+
+#Generators
+def my_range(n):
+    count = 0
+    while count < n:
+        yield count 
+        count += 1
+
+
+# for i in my_range(4):
+#     print(i)
+# for i in range(4):
+#     print(i)
+
+my = my_range(4)
+print(next(my))
+print(next(my))
+print(next(my))
+print(next(my))
+# print(next(my))
+
+# 0 1 2 3 2 1 0
+def my_special_range(n):
+    count = 0
+    while count < n:
+        yield count 
+        count += 1
+    count -= 2
+    while count >= 0 :
+        yield count 
+        count -= 1
+
+for i in my_special_range(4):
+    print(i)
+
+
+
+
+# print()
+# print()
+# #generator expressions
+# l=[x*3 for x in range(1000)]
+# g=(x*3 for x in range(2))
+
+# for i in g:
+#     print(i)
+
+# print(next(g))
+# # print(next(g))
+# # print(next(g))
+
+
+
+
+# Exercise - Iterator
+
+# class WordIterator:
+#     def __init__(self,sentence):
+#         self.index=0
+#         self.wlist=sentence.split()
+#     # write function __iter__ and __next__
+#     def __iter__(self):
+#         return self
+    
+#     def __next__(self):
+#         pass
+    
+
+# wi=WordIterator("Welcome to python programming classes")  
+
+# for i in wi:
+#     print(i)
